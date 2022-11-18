121ab1eb5444b2f8f49a3a28c469f3041e83bd12
Fernanda-15 <fernandaqp15@gmail.com>
2022-11-18 16:28:51 -0600

    Quitar opción agregar

--- src/components/schedules/SchedulesList.jsx
+++ src/components/schedules/SchedulesList.jsx
@@ -17,19 +17,7 @@ function ScheduleList() {
                 Gestión de Horarios
               </h2>
             </div>
-            <div className="mt-5 flex lg:mt-0 lg:ml-4">
-              <span className="hidden sm:block">
-                <Link to="#">
-                  <button
-                    type="button"
-                    className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    <IoPencil className="-ml-1 mr-2 h-5 w-5 text-gray-500" />
-                    Guardar
-                  </button>
-                </Link>
-              </span>
-            </div>
+         
           </div>
           <div className="shadow overflow-hidden border-b border-gray-200 rounded-lg">
             <table className="w-full divide-y divide-gray-200">
